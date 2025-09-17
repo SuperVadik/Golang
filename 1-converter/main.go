784ff257293d18665883c121067d911d8d3dccea
andrei <ml@gmail.com>
2025-09-17 19:32:28 +0300

    strings-and-functions

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -9,3 +9,13 @@ func main() {
 	eurRub := usdRub / usdEur
 	fmt.Print(eurRub)
 }
+
+func readCurrency() string {
+	var val string
+	fmt.Scan(&val)
+	return val
+}
+
+func calculate(number float64, orignCurr, targetCurr string) float64 {
+	return 0
+}
