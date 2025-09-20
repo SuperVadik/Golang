424adb442c7363c6063f69d2ee18402fe7c8bb30
andrei <ml@gmail.com>
2025-09-20 17:56:11 +0300

    checkRepeatCalculation

--- go-demo/main.go
+++ go-demo/main.go
@@ -3,20 +3,26 @@ package main
 import (
 	"fmt"
 	"math"
+	"strings"
 )
 
 const power float64 = 2
 
 func main() {
-	for i := 0; i < 10; i++ {
-		fmt.Printf("%d\n", i)
-	}
-
 	fmt.Println("Калькулятор индекса массы тела")
-	userHeight, userKg := getUserInput()
-	imt := calculateImt(userHeight, userKg)
+	for {
+		userHeight, userKg := getUserInput()
+		imt := calculateImt(userHeight, userKg)
+		outputResult(imt)
+		if !checkRepeatCalculation() {
+			break
+		}
+	}
+}
 
-	outputResult(imt)
+func outputResult(imt float64) {
+	result := fmt.Sprintf("Ваш ИМТ: %.0f\n", imt)
+	fmt.Println(result)
 	switch {
 	case imt < 16:
 		fmt.Println("У вас сильный дефицит массы тела")
@@ -31,13 +37,10 @@ func main() {
 	}
 }
 
-func outputResult(imt float64) {
-	fmt.Printf("Ваш ИМТ: %.0f\n", imt)
-}
-
 func calculateImt(userHeight, userKg float64) float64 {
 	return userKg / math.Pow(userHeight/100, power)
 }
+
 func getUserInput() (float64, float64) {
 	var userHeight, userKg float64
 	fmt.Print("Введите свой рост в сантиметрах: ")
@@ -45,4 +48,11 @@ func getUserInput() (float64, float64) {
 	fmt.Print("Введите свой вес: ")
 	fmt.Scan(&userKg)
 	return userHeight, userKg
+}
+
+func checkRepeatCalculation() bool {
+	var userChoise string
+	fmt.Print("Вы хотите выполнить ещё расчёт? (y/n)")
+	fmt.Scan(&userChoise)
+	return strings.ToUpper(userChoise) == "Y"
 }
