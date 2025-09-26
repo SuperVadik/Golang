12a04d48bcce7241d5b3fb10d6240b1bee1bfe2d
andrei <ml@gmail.com>
2025-09-26 10:25:05 +0300

    append

--- go-demo-2/main.go
+++ go-demo-2/main.go
@@ -3,9 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	transactions := []int{0, 20, 35}
-	temp := transactions
-	transactions = append(transactions, 100)
-	fmt.Println(temp)
+	transactions := []float64{}
+	for {
+		transaction := scanTransaction()
+		if transaction == 0 {
+			break
+		}
+		transactions = append(transactions, transaction)
+	}
 	fmt.Println(transactions)
+}
+
+func scanTransaction() float64 {
+	var transaction float64
+	fmt.Print("Введите транзакцию (n для выхода): ")
+	fmt.Scan(&transaction)
+	return transaction
 }
