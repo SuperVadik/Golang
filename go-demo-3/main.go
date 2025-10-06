00800839bf2b927261c45242aafc0ff1a9afa325
andrei <ml@gmail.com>
2025-10-06 09:36:20 +0300

    map add/delete

--- go-demo-3/main.go
+++ go-demo-3/main.go
@@ -4,8 +4,15 @@ import "fmt"
 
 func main() {
 	m := map[string]string{
-		"NY": "NewYork",
-		"IZ": "Izhevsk",
+		"PurpleScool": "purplescool.ru",
 	}
-	fmt.Println(m["NY"])
+	fmt.Println(m)
+	fmt.Println(m["PurpleScool"])
+	m["PurpleScool"] = "https://purpleschool.ru"
+	fmt.Println(m)
+	m["Google"] = "https://google.com"
+	m["Yandex"] = "https://yandex.ru"
+	fmt.Println(m)
+	delete(m, "Yandex")
+	fmt.Println(m)
 }
